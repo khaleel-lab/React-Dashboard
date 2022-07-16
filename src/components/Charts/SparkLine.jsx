06341c0f2f4e14397453b57466ca3d1131dca7b6
dbasha <dudekula.basha@capgemini.com>
2022-07-16 07:53:12 +0530

    Revenue Section

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import {
+	SparklineComponent,
+	Inject,
+	SparklineTooltip,
+} from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
-	return <div>SparkLine</div>;
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+	{
+		console.log(data);
+	}
+	return (
+		<SparklineComponent
+			id={id}
+			height={height}
+			width={width}
+			LineWidth={1}
+			valueType='Numeric'
+			fill={color}
+			border={{ color: currentColor, width: 2 }}
+			dataSource={data}
+			xName='x'
+			yName='yval'
+			type={type}
+			tooltipSettings={{
+				visible: true,
+				format: '${x} : data ${yval}',
+				trackLineSettings: {
+					visible: true,
+				},
+			}}
+		>
+			<Inject services={[SparklineTooltip]} />
+		</SparklineComponent>
+	);
 };
 
 export default SparkLine;
