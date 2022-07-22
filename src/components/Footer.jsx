fbb4d0d8447ed15c424ddc89d601c1f0252006e6
dbasha <dudekula.basha@capgemini.com>
2022-07-22 22:39:54 +0530

    Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	const CurrentYear = new Date().getFullYear();
+	return (
+		<div className='mt-24'>
+			<p className='dark:text-gray-200 text-gray-700 text-center m-20'>
+				© {CurrentYear} All rights reserved by LazyCoderLee
+			</p>
+		</div>
+	);
 };
 
 export default Footer;
