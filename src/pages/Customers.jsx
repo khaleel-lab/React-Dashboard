62bb16c16e4fd934b285eb4377d8fecf50fd67a0
dbasha <dudekula.basha@capgemini.com>
2022-07-22 22:46:41 +0530

    Customers Page Modifications

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import {
 	GridComponent,
-	ColumnDirective,
 	ColumnsDirective,
+	ColumnDirective,
+	Page,
 	Selection,
+	Inject,
+	Edit,
 	Toolbar,
 	Sort,
 	Filter,
-	Page,
-	Edit,
-	Inject,
 } from '@syncfusion/ej2-react-grids';
+
 import { customersData, customersGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Customers = () => {
-	return (
-		<div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
-			<Header category='Page' title='Customers' />
+	const selectionsettings = { persistSelection: true };
+	const toolbarOptions = ['Delete'];
+	const editing = { allowDeleting: true, allowEditing: true };
 
+	return (
+		<div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+			<Header category='Page' title='Customers' />
 			<GridComponent
 				dataSource={customersData}
+				enableHover={false}
 				allowPaging
+				pageSettings={{ pageCount: 5 }}
+				selectionSettings={selectionsettings}
+				toolbar={toolbarOptions}
+				editSettings={editing}
 				allowSorting
-				toolbar={['Delete']}
-				editSettings={{ allowDeleting: true, allowEditing: true }}
-				width='auto'
 			>
 				<ColumnsDirective>
 					{customersGrid.map((item, index) => (
 						<ColumnDirective key={index} {...item} />
 					))}
 				</ColumnsDirective>
-				<Inject services={[Sort, Filter, Page, Edit, Toolbar, Selection]} />
+				<Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
 			</GridComponent>
 		</div>
 	);
